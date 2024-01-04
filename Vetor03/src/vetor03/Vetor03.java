dcc22c9496436364eb08bd032ad121a9d776425a
LuisFelipe-MB <109697962+LuisFelipe-MB@users.noreply.github.com>
2024-01-04 18:34:16 -0300

    novo exercicio

---
+++ Vetor03/src/vetor03/Vetor03.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetor03;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Vetor03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double v[] = {3.6, 6, 2.9, 7, 0.3};
+        Arrays.sort(v);
+        for (double valor: v){
+            System.out.print(valor + " ");
+        }
+    }
+    
+}
